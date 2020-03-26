5422ca4efe9ee7bb4670b16f6b4937743eb41312
Jake Rathman <jake.rathman@gmail.com>
2020-03-25 21:16:25 -0500

    Updates

---
+++ precache-manifest.d2430bb2628ad6afe1bc388730e209bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd058641097056d6a9ac4bb6921506b2",
+    "url": "/covid-19-visualized/index.html"
+  },
+  {
+    "revision": "226e940cf897e8ee07bb",
+    "url": "/covid-19-visualized/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7e44ae221868545cb4f",
+    "url": "/covid-19-visualized/static/js/2.491618c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-visualized/static/js/2.491618c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226e940cf897e8ee07bb",
+    "url": "/covid-19-visualized/static/js/main.704ee697.chunk.js"
+  },
+  {
+    "revision": "59a452179e42421a8d18",
+    "url": "/covid-19-visualized/static/js/runtime-main.a2f35f12.js"
+  }
+]);
